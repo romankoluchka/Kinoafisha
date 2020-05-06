8f3acf8157ee8fbacfe603a4af80b6d0e739db94
Roman Luchka <luchkaroman@gmail.com>
2020-05-06 20:49:06 +0300

    install Material-UI

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 import App from "./components/App";
+import { ThemeProvider, CssBaseline } from "@material-ui/core";
+import theme from './theme'
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   rootElement
 );
